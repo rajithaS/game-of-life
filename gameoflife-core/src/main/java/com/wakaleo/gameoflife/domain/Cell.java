698ad1d4de96a8fbced7f001b2b011c86282535a
Rajitha Siriwardena <raji.truth@live.com>
2012-08-27 11:38:35 +0530

    5th change

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("+"), DEAD_CELL(".");//4th update
+    LIVE_CELL("*"), DEAD_CELL(".");//4th update
 
     private String symbol;
 
